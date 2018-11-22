883f0d0d8fba095d26ade77bf4e21e0c59f06322
nagaminenot <nagamine@slogan.jp>
2018-11-22 12:24:02 +0900

    大本の基盤作成

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# scraping_list
+# Scraping List
+## 初回セットアップ
+Python3が入っている環境で以下を実行
+`pip install -r requirements.txt`
+
+## 使い方
+1. `python (実行したいpythonファイル)`
+2. URLをベタ打ち
+3. result_1~xx.csvが作成される
+4. linuxのコンソール画面より cat result_*.csv > result.csv を実行し、csvを一つにまとめる
+
---
+++ khmer24.py
@@ -0,0 +1,39 @@
+from tqdm import tqdm
+from bs4 import BeautifulSoup
+import urllib.request
+import re
+
+# 対象のURL入力
+print("スクレイピングしたいURLを入力して下さい")
+url = str(input(">>> "))
+result = ''
+
+# urlからsoup(lxml形式のページ内容)を返すメソッド
+def url_to_soup(url):
+    req = urllib.request.Request(url)
+    response = urllib.request.urlopen(req)
+    html = response.read()
+    soup = BeautifulSoup(html, "lxml")
+    return soup
+
+
+    file_name = "./result.csv"
+    try:
+        file = open(file_name, 'w', encoding='utf-16')
+        file.write(str(result))
+    except Exception as e:
+        print(e)
+    finally:
+        file.close()
+    count += 1;
+
+def write_csv (result) :
+  file_name = "./result.csv"
+    try:
+        file = open(file_name, 'w', encoding='utf-16')
+        file.write(str(result))
+    except Exception as e:
+        print(e)
+    finally:
+        file.close()
+    count += 1;
